9b4629e7bf009afd8234c5b9164b0f00e5aa8627
doha <dohaahi@naver.com>
2023-02-21 02:37:55 +0900

    use components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Expenses from "./Expenses";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      {/* props.date는 App에서 부터 전해 받아온 data(정확히 말하자면 date)를 ExpenseDate에 전달해주기 위해 작성된 코드 */}
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.97</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
