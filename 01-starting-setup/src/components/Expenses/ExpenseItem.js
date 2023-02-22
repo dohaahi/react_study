e1061e84531a2ca105b9f231a8ad05fbd46bb1eb
doha <dohaahi@naver.com>
2023-02-22 17:01:05 +0900

    component file

---
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./UI/Card";
+
+export default function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      {/* props.date는 App에서 부터 전해 받아온 data(정확히 말하자면 date)를 ExpenseDate에 전달해주기 위해 작성된 코드 */}
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
