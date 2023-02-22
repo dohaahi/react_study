f8dfd7fb5d41a90f20d4a081175ff70edc82b8de
doha <dohaahi@naver.com>
2023-02-22 23:26:16 +0900

    add eventHandler

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!!!");
+  };
+
   return (
     <Card className="expense-item">
       {/* props.date는 App에서 부터 전해 받아온 data(정확히 말하자면 date)를 ExpenseDate에 전달해주기 위해 작성된 코드 */}
@@ -11,6 +15,7 @@ export default function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
