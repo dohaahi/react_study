2bdce4571f197a2de45432bf5699087dda0cce25
doha <dohaahi@naver.com>
2023-02-22 16:51:47 +0900

    wrapper component: Card

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Expenses from "./Expenses";
+import Card from "./Card";
 
 export default function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* props.date는 App에서 부터 전해 받아온 data(정확히 말하자면 date)를 ExpenseDate에 전달해주기 위해 작성된 코드 */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
