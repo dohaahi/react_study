f9887548e7b241903fbcb686bc5f7ca464e57d4a
doha <dohaahi@naver.com>
2023-02-22 17:05:50 +0900

    file

--- 01-starting-setup/src/components/UI/Card.js
+++ 01-starting-setup/src/components/UI/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 // item과 item을 둘러싼 expenses의 div태그를 Card 태그로 바꾸기 위한 컴포넌트
 export default function Card(props) {
   // classes는  card.css 뿐만 아니라 expenseItem.css를 동적으로 가져오기 위함
-  // 'card' 뒤에 공백이 포함되어야 2개의 class가 인식됨
+  // 'card' 뒤에 공백이 포함되어야 2개의 class가 인식
   const classes = "card " + props.className;
 
   return <div className={classes}>{props.children}</div>;
