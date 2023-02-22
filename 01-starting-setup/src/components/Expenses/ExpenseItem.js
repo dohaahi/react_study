d6aa6cb51b5bb995ea3fa9ba644fd2a32fbd98d0
doha <dohaahi@naver.com>
2023-02-23 01:57:24 +0900

    useState

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked!!!!");
+    setTitle("updated!!");
   };
 
   return (
@@ -12,7 +15,7 @@ export default function ExpenseItem(props) {
       {/* props.date는 App에서 부터 전해 받아온 data(정확히 말하자면 date)를 ExpenseDate에 전달해주기 위해 작성된 코드 */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
