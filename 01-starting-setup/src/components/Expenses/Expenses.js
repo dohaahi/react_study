d34f6e53e17b7c97e0460bf67bcb3df78bc09027
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-02-25 14:52:10 +0900

    use && operator

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -23,9 +23,8 @@ export default function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         />
         {/* filteredExpenses가 없다면 사용자가 알 수 있도록 화면에 표시해주기 */}
-        {filteredExpenses.length === 0 ? (
-          <p>no expenses found!</p>
-        ) : (
+        {filteredExpenses.length === 0 && <p>no expenses found!</p>}
+        {filteredExpenses.length > 0 &&
           filteredExpenses.map((expenses) => (
             <ExpenseItem
               key={expenses.id}
@@ -33,8 +32,7 @@ export default function Expenses(props) {
               amount={expenses.amount}
               date={expenses.date}
             />
-          ))
-        )}
+          ))}
       </Card>
     </div>
   );
