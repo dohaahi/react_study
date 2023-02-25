87ea3b00bd5ee2fe22a515389e442c4798db46c1
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-02-25 17:07:19 +0900

    add editing button

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,7 @@ export default function ExpenseForm(props) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [cancel, setCancel] = useState("");
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -29,6 +30,10 @@ export default function ExpenseForm(props) {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+  };
+
+  const cancelHandler = () => {
+    setCancel("cancel Submit");
   };
 
   return (
@@ -64,6 +69,9 @@ export default function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 export default function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDateHandler = (enteredExpenseData) => {
     const expenseDate = {
       ...enteredExpenseData,
@@ -9,11 +12,31 @@ export default function NewExpense(props) {
     };
 
     props.onAddExpense(expenseDate);
+    // form이 제출되고 나면 초기 상태로 돌아가게함
+    setIsEditing(false);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDateHandler} />
-    </div>
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  let buttonContent = (
+    <button onClick={startEditingHandler}>Add New Expense</button>
   );
+
+  // Add New Expense버튼을 누르면 addForm 보여주기
+  if (isEditing) {
+    buttonContent = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDateHandler}
+        onCancel={stopEditingHandler}
+      />
+    );
+  }
+
+  return <div className="new-expense">{buttonContent}</div>;
 }
