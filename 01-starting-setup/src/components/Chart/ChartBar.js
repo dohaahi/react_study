27701874a347a262ff1994f3f5c095de1688110d
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-02-28 00:54:31 +0900

    create Chart component & add height style

---
+++ 01-starting-setup/src/components/Chart/ChartBar.js
@@ -0,0 +1,23 @@
+import "./ChartBar.css";
+
+export default function ChartBar(props) {
+  let barFillHeight = "0%";
+
+  // barFillHeight가 0 초과라면 value를 퍼센트로 나타냄
+  if (props.max > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          // style이 동적으로 변해야하기 때문에 자바스크립트 객체를 사용
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label} </div>
+    </div>
+  );
+}
