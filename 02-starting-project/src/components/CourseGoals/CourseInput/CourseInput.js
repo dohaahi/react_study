6ee10bf4fdfd5a22351f0c7779955e00aa2001ca
[AI6/2반] 김도하 <dohaahi@naver.com>
2023-03-01 19:13:39 +0900

    inline styling

--- 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  // 사용자가 입력한 값이 옮은 값인지 확인하기 위한 코드
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    // 사용자가 입력한 값이 유효하다면 기존 스타일 적용
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    // 만약 사용자가 공백을 입력하면 실행 X
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
