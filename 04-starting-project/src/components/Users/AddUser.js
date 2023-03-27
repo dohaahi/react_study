6b095448faa8796febd2d4c478cd38ff963e5af3
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-27 12:18:11 +0900

    add AddUser component

---
+++ 04-starting-project/src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUSer = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      {/* form이 제출될 때 실행될 함수를 obSubmit props로 지정 */}
+      <form onSubmit={addUserHandler}>
+        {/* label에 htmlFor 속성을 줘서 input과 연결 */}
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age(Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUSer;
