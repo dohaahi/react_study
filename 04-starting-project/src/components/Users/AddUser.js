37a210230ae52e4b0bbfa853206eca752180a533
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-27 14:23:38 +0900

    add onChange event Handler

--- 04-starting-project/src/components/Users/AddUser.js
+++ 04-starting-project/src/components/Users/AddUser.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
@@ -13,9 +26,9 @@ const AddUser = (props) => {
       <form onSubmit={addUserHandler}>
         {/* label에 htmlFor 속성을 줘서 input과 연결 */}
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button>Add User</Button>
       </form>
     </Card>
