487e6e7b262ef3bc7f8770470a0dfb05279e6d60
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-27 14:12:25 +0900

    use Button component

--- 04-starting-project/src/components/Users/AddUser.js
+++ 04-starting-project/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -15,7 +16,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age(Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button>Add User</Button>
       </form>
     </Card>
   );
