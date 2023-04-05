6352fed12244afce6a139d69db32cae94d75e1ac
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-04-06 02:02:05 +0900

    add ErrorModal component

---
+++ 04-starting-project/src/components/UI/ErrorModal.js
@@ -0,0 +1,19 @@
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
+
+export const ErrorModal = (props) => {
+  return (
+    <Card>
+      <header className={classes.modal}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button>OK</Button>
+      </footer>
+    </Card>
+  );
+};
